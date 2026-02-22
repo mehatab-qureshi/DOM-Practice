23aaf624e160a86c6ba364195bc7661626e4f1bd
Mehatab Qureshi <mahetabqureshi5676@gmail.com>
2026-02-22 23:21:54 +0530

    js practice

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
-let mypara = document.querySelector(".demo");
+//selecting ele
+// let mypara = document.querySelectorAll(".demo");
+// mypara[1].innerHTML = "This is dom";
 
-mypara.innerHTML = "This is dom";
+// //forEach method
+// mypara.forEach((para) => {
+//   para.innerHTML = "This is dom";
+// });
+
+// //accessing ele content
+// let mypara = document.querySelector(".demo");
+// // mypara.innerHTML = "<b>hello</b>hello";
+// mypara.innerText = "<b>hello</b>hellooo";
+
+//accessing attributes
+let input = document.getElementById("username");
+console.log(input.value);
+console.log(input.type);
+console.log(input.id);
+
+let div = document.querySelector("div");
+// console.log(div.data - role); //so to overcome this we have concept called getAttribute & setAttribute
+
+console.log(div.getAttribute("data-role"));
+div.setAttribute("data-role", "hello");
+console.log(div.getAttribute("data-role"));
+
+//diff btw .val n getAttr
+console.log(input.value);
+input.value = "mehatab qureshi";
+console.log(input.value);
+
+console.log(input.getAttribute("value"));
