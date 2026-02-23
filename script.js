604cb91cfc6dcac9a93e0796fa848b7bd29270a2
Mehatab Qureshi <mahetabqureshi5676@gmail.com>
2026-02-23 23:20:26 +0530

    styles attr

--- script.js
+++ script.js
@@ -12,22 +12,28 @@
 // // mypara.innerHTML = "<b>hello</b>hello";
 // mypara.innerText = "<b>hello</b>hellooo";
 
-//accessing attributes
-let input = document.getElementById("username");
-console.log(input.value);
-console.log(input.type);
-console.log(input.id);
+// //accessing attributes
+// let input = document.getElementById("username");
+// console.log(input.value);
+// console.log(input.type);
+// console.log(input.id);
 
-let div = document.querySelector("div");
-// console.log(div.data - role); //so to overcome this we have concept called getAttribute & setAttribute
+// let div = document.querySelector("div");
+// // console.log(div.data - role); //so to overcome this we have concept called getAttribute & setAttribute
 
-console.log(div.getAttribute("data-role"));
-div.setAttribute("data-role", "hello");
-console.log(div.getAttribute("data-role"));
+// console.log(div.getAttribute("data-role"));
+// div.setAttribute("data-role", "hello");
+// console.log(div.getAttribute("data-role"));
 
-//diff btw .val n getAttr
-console.log(input.value);
-input.value = "mehatab qureshi";
-console.log(input.value);
+// //diff btw .val n getAttr
+// console.log(input.value);
+// input.value = "mehatab qureshi";
+// console.log(input.value);
 
-console.log(input.getAttribute("value"));
+// console.log(input.getAttribute("value"));
+
+//style property
+
+let box = document.querySelector(".box");
+box.style.backgroundColor = "blue";
+box.style.width = "200px";
